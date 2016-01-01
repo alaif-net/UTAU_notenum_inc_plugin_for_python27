a235f0d58abce3c5d38c5094b6e3286d05ca0247
alaif <projectalaif@gmail.com>
2016-01-01 13:52:25 +0900

    FIX: - デバッグ時のprintが残ってたので削除 - 状態変数に関数を保持するように変更

--- notenum_inc_plugin.py
+++ notenum_inc_plugin.py
@@ -10,9 +10,6 @@ import sys
 
 #一時ファイル名
 WORKING_FILE='.working'
-#状態
-STATE_WAIT_SECTION = 0      #セクション開始待ち
-STATE_WAIT_NOTENO_ENTRY = 1 #NoteNumエントリー待ち
 
 #「セクション開始待ち」状態の行処理
 def state_wait_section_proc(line, file_output):
@@ -33,9 +30,7 @@ def state_wait_noteno_entry_proc(line, file_output):
 			noteno = int(fields[1])
 			if noteno < 108:
 				#NoteNumの上限に達していなかった
-				print 'NoteNum=%d' % noteno
 				noteno = noteno + 1
-				print 'NoteNum=%d' % noteno
 				file_output.write('NoteNum=%d\n' % noteno)
 			else:
 				#NoteNumの上限に達していた
@@ -50,35 +45,35 @@ def state_wait_noteno_entry_proc(line, file_output):
 		file_output.write(line)
 		return STATE_WAIT_NOTENO_ENTRY #「NoteNumエントリー待ち」のまま
 
-#状態ごとの行処理関数のリスト
-STATE_PROC = {
-	STATE_WAIT_SECTION     : state_wait_section_proc,
-	STATE_WAIT_NOTENO_ENTRY: state_wait_noteno_entry_proc
-}
+#状態
+STATE_WAIT_SECTION = state_wait_section_proc           #セクション開始待ち
+STATE_WAIT_NOTENO_ENTRY = state_wait_noteno_entry_proc #NoteNumエントリー待ち
 
-state = STATE_WAIT_SECTION #最初の状態を「セクション開始待ち」に設定
+#最初の状態を「セクション開始待ち」に設定
+state_func = STATE_WAIT_SECTION
 
+### メイン関数
 if __name__ == "__main__":
 	#コマンドライン引数を確認
 	if len(sys.argv) < 2:
 		print 'Usage: %s filename' % sys.argv[0]
 		quit()
-	
+
 	#ファイルの存在確認
 	path_pipe_file = sys.argv[1]
 	if os.path.isfile(path_pipe_file) == False:
 		print 'Error: Pipe file not found. %s' % path_pipe_file
 		quit()
-	
+
 	#ファイルオープン
 	file_input = open(path_pipe_file, "r")
 	file_output = open(WORKING_FILE, "w")
 	for line in file_input:
 		#読み込んだ行を処理
-		state = STATE_PROC[state](line, file_output)
-	
+		state_func = state_func(line, file_output)
+
 	file_output.close()
 	file_input.close()
-	
+
 	#出力したファイルで入力ファイルを上書き
 	shutil.move(WORKING_FILE, path_pipe_file)
